54dca85dd42ba21630f982708ac89c4967f2f9f8
MrLeKiet <kietsvis@gmail.com>
2025-09-10 14:22:04 +0700

    Setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Page } from "zmp-ui";
+
+
+function HomePage() {
+  return (
+    <Page>
+    </Page>
+  );
+}
+
+export default HomePage;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  darkMode: ["selector", '[zaui-theme="dark"]'],
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.{js,jsx,ts,tsx,vue}"],
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ["Roboto Mono", "monospace"],
+      },
+      spacing: {
+        st: "var(--safe-top)",
+        sb: "var(--safe-bottom)",
+      },
+    },
+  },
+};
